c174b21f47396cc4e7437135434f8822d99fa580
Felipe Brandão de Figueiredo <128430670+FelpsBrands@users.noreply.github.com>
2023-04-06 18:17:12 -0300

    Add files via upload

---
+++ Exercício 1016/DistanciaCarros.java
@@ -0,0 +1,19 @@
+package distanciacarros;
+import java.util.Scanner;
+
+
+public class DistanciaCarros {
+
+public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
+    int distancia, tempo;
+    distancia = input.nextInt();
+    tempo = distancia*2;
+    System.out.println(tempo+" minutos");
+    
+    
+        
+      
+    }
+    
+}
