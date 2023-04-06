b23c4864249f027a9498bab77ee46a5763deba83
Felipe Brandão de Figueiredo <128430670+FelpsBrands@users.noreply.github.com>
2023-04-06 18:05:21 -0300

    Add files via upload

---
+++ Exercício 1003/SomaSimples.java
@@ -0,0 +1,26 @@
+
+package somasimples;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author figue
+ */
+public class SomaSimples {
+ 
+   
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        int A = input.nextInt();
+        
+        int B = input.nextInt();
+        
+        int SOMA = B+A;
+        
+        System.out.println("SOMA = " + SOMA );
+        
+    }
+    
+}
